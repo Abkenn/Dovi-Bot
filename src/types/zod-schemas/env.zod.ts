3c90eae1bbb62c0f56292c7eea8f727c9ef9a6b4
Abken <27154073+Abkenn@users.noreply.github.com>
2026-04-03 21:21:59 +0300

    Update env schema

--- src/types/zod-schemas/env.zod.ts
+++ src/types/zod-schemas/env.zod.ts
@@ -4,6 +4,10 @@ export const envSchema = z.object({
   DISCORD_TOKEN: z.string().min(1),
   DISCORD_CLIENT_ID: z.string().min(1),
   DISCORD_GUILD_ID: z.string().min(1),
+  DISCORD_STAGING_ENV_GUILD_ID: z.string().min(1),
+  DISCORD_PROD_ENV_GUILD_ID: z.string().min(1),
+  DATABASE_URL: z.string().min(1),
+  DIRECT_URL: z.string().min(1),
 });
 
 export const env = envSchema.parse(process.env);
